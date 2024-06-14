3159633d3cc57d32763061aff2e7b45d14fb657e
AMG <146374366+SARTDv@users.noreply.github.com>
2024-06-14 16:43:18 -0500

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,23 @@ from app.auth.models import User,Client,Userm,Delivery
 easydel =  create_app("prod")
 with easydel.app_context():
     db.create_all()
+    
+    if not Delivery.query.filter_by(delivery_name="testD").first():
+        Delivery.create_delivery(
+            name="testDelivery",
+            address="test adress",
+            email="test-testing@test.com",
+            telephone="test**123"
+        ) 
+          
+    if not Userm.query.filter_by(userm_name="test_APARECE").first():
+        Userm.create_userm(
+            user="test_APARECE",
+            email="testA-testing@test.com",
+            password="testA**123",
+            address="test-address",
+            deliveryid=1
+        )
     if not Client.query.filter_by(client_name="testclient").first():
         Client.create_client(
             name="testclient",
@@ -20,23 +37,5 @@ with easydel.app_context():
             password="test**123",
             address="test-address",
             clientid=1
-        ) 
-
-    if not Delivery.query.filter_by(delivery_name="testD").first():
-        Delivery.create_delivery(
-            name="testDelivery",
-            address="test adress",
-            email="test-testing@test.com",
-            telephone="test**123"
-        ) 
-          
-    if not Userm.query.filter_by(user_name="test_APARECE").first():
-        Userm.create_userm(
-            user="test_APARECE",
-            email="testA-testing@test.com",
-            password="testA**123",
-            address="test-address",
-            deliveryid=1
-        )
-    
+        )    
 easydel.run()
